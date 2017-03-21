87dbe350089794526c400aeaa4f1f7267675696e
gabriele.mecella@tin.it <mecbar@arrakis.homenet.telecomitalia.it>
2017-03-21 03:10:27 +0100

    angul

---
+++ js/app.js
@@ -0,0 +1,48 @@
+'use strict';
+(function() {
+
+  var app = angular.module('mecbar', [])
+  ;
+
+     
+    app.controller('MenuGenerale', function($scope) {
+     $scope.test="";
+
+    $('input').html('   ');
+
+    var nome = function($scope) {
+      $scope.test = 'testa' ;
+     }
+    })
+});    
+    
+   /* 
+    
+    $scope.valori = {
+     Inserimento: '1',
+     Lettura : '2',
+     Modifica : '3',
+     Cancellazione: '4',
+     Ricerca : '5',
+     Varie : '6',
+     }
+
+
+     
+     
+    
+     $('input').html('   ');
+    
+     this.addImporto = function(operaz) {
+       menu = this.operaz;
+              
+       
+      /*   this.set = function(risultato) {
+            this.risultato = risultato; */
+                    
+    
+ 
+
+    
+
+  
