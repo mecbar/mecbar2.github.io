493cbe1b6dbb3fecf502181fb35da2f3e9c652d2
gabriele.mecella@tin.it <mecbar@arrakis.homenet.telecomitalia.it>
2017-03-23 00:04:38 +0100

    tre

--- js/app.js
+++ js/app.js
@@ -1,21 +1,65 @@
 'use strict';
 (function() {
 
- var app = angular.module('mecbar', []) 
+  angular.module('mecbar', [])
+    .controller('SituazioneGenerale', function($scope) {
+       $scope.valori = {
+       cassa: '0.00',
+        oncard: '0.00',
+      alice: '6621.45',
+      alessandro: '8566.45',
+      descrizione: 'altri depositi',
+     }
 
+   var imp = {}; 
+  }).controller('ShowController' , function() {
+    this.risultato = false ;
+
+    this.set = function(risultato) {
+          this.risultato = risultato;
+          }
+
+  } ).controller('ReviewController', [ '$http', function($http) {
+    
+     var rev = this;
+     this.operaz = [];
+     var imp = {};
+     $('input').html('   ');
+    
+     this.addImporto = function(imp) {
+        this.operaz = [];
+       imp = this.importo;
+       this.ciao = imp;
+         this.risultato = false ;
+
+         this.set = function(risultato) {
+            this.risultato = risultato;
+         }      
+                    
+       rev.operaz = [];
      
-  app.controller('MenuCtrl', ['$scope' , function($scope) {
-     $scope.operationList=[];
-
-     $scope.addOperation = function(operation) {
+      $http({
+      method : 'GET' ,
+      /* 
+      url : '/provajax',
+      */
+      url : '/api/v1/ccbm/',
+      data : this.importo ,
+      }).success(function(resp) {
+            
+         rev.operaz = resp['objects'];
+        /*  
+        
+       var  impo = imp.toString();
+        
+       */
        
-     /*  $scope.operationList.push({ name : operation.name});
-     */  
-       $scope.operationList.push($scope.operationList.length)
-       $scope.operation=[];
          
-      
-      };
-  } ] )
-    }
-);
+            } ); 
+     } 
+     }
+     
+  ]);
+
+}) ();
+  
