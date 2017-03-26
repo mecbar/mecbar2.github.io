ed63c88351950f282006e1b9d3ffa9076d8e6db8
gabriele.mecella@tin.it <mecbar@arrakis.homenet.telecomitalia.it>
2017-03-26 13:14:28 +0200

    angular

--- js/app.js
+++ js/app.js
@@ -1,65 +1,15 @@
-'use strict';
 (function() {
-
-  angular.module('mecbar', [])
-    .controller('SituazioneGenerale', function($scope) {
-       $scope.valori = {
-       cassa: '0.00',
-        oncard: '0.00',
-      alice: '6621.45',
-      alessandro: '8566.45',
-      descrizione: 'altri depositi',
-     }
-
-   var imp = {}; 
-  }).controller('ShowController' , function() {
-    this.risultato = false ;
-
-    this.set = function(risultato) {
-          this.risultato = risultato;
-          }
-
-  } ).controller('ReviewController', [ '$http', function($http) {
-    
-     var rev = this;
-     this.operaz = [];
-     var imp = {};
-     $('input').html('   ');
-    
-     this.addImporto = function(imp) {
-        this.operaz = [];
-       imp = this.importo;
-       this.ciao = imp;
-         this.risultato = false ;
-
-         this.set = function(risultato) {
-            this.risultato = risultato;
-         }      
-                    
-       rev.operaz = [];
-     
-      $http({
-      method : 'GET' ,
-      /* 
-      url : '/provajax',
-      */
-      url : '/api/v1/ccbm/',
-      data : this.importo ,
-      }).success(function(resp) {
-            
-         rev.operaz = resp['objects'];
-        /*  
-        
-       var  impo = imp.toString();
-        
-       */
-       
-         
-            } ); 
-     } 
-     }
-     
-  ]);
-
+  'use strict';
+ angular.module('mecbar', [])
+ .controller('NationListCtrl', function ($scope,$http) {
+    $http({
+        method: 'GET',
+        url : 'data/nations.json'
+    }).then(function (data) {
+          $scope.nations = data ;
+       } , function (data) {
+         console.log('Error file nations non caricato');
+       } ); 
+    } );
 }) ();
   
